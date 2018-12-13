88c820243da3a325648bcf137d144fd44960eb54
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-12-13 08:51:30 -0600

    Update GeneratorService.js

--- src/core/GeneratorService.js
+++ src/core/GeneratorService.js
@@ -1,7 +1,7 @@
 'use strict';
 var utilities = require('./utilities.js');
 var Entity = require('../entity');
-var gameServer = require('./Gameserver.js');
+
 module.exports = class GeneratorService {
   constructor(gameServer) {
     this.gameServer = gameServer;
@@ -64,7 +64,7 @@ module.exports = class GeneratorService {
   spawnFood() {
     this.foodSpawned++;
     let f = new Entity.Food(this.gameServer.getWorld().getNextNodeId(), null, utilities.getRandomPosition(this.config.borderRight, this.config.borderLeft, this.config.borderBottom, this.config.borderTop), this.config.foodMass, this.gameServer);
-    f.setColor(gameServer.getRandomColor());
+    f.setColor(this.gameServer.getRandomColor());
 
     this.gameServer.addNode(f);
     this.gameServer.currentFood++;
