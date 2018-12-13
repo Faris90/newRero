b09e2b604ec857eb8648f28d3f085c0634445522
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-12-13 09:11:03 -0600

    Update Cell.js

--- src/entity/Cell.js
+++ src/entity/Cell.js
@@ -215,7 +215,7 @@ Cell.prototype.calcMovePhys = function (config) {
   var cos = Math.cos(this.angle);
   if (this.cellType == 3) {
     //movement and collision check for ejected mass cells
-   /* var collisionDist = r * 2; // Minimum distance between the 2 cells (allow cells to go a little inside eachother before moving them a.k.a cell squishing)
+    var collisionDist = r * 2; // Minimum distance between the 2 cells (allow cells to go a little inside eachother before moving them a.k.a cell squishing)
     var maxTravel = r; //check inbetween places for collisions (is needed when cell still has high speed) - max inbetween move before next collision check is cell radius
     var totTravel = 0;
     var xd = 0;
@@ -246,7 +246,7 @@ Cell.prototype.calcMovePhys = function (config) {
               this.gameServer.getWorld().setNodeAsMoving(cell.getId(), cell);
               //if (!this.gameServer.getMovingNodes().has(cell.getId())) {
               //  this.gameServer.setAsMovingNode(cell.getId());
-              //}*/
+              //}
             } 
             if (this.moveEngineTicks == 0) {
               this.setMoveEngineData(0, 1); //make sure a collided cell checks again for collisions with other cells
